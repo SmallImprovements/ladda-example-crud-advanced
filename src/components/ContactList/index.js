1f5b3d343893cdc479826d3b13956f60d8b7059d
LFDM <1986gh@gmail.com>
2017-04-25 20:41:07 +0200

    Update contacts inline

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,7 +1,8 @@
-import { Link } from 'react-router';
+import { Component } from 'react';
 import { flow, map } from 'lodash';
 import { Card } from 'components/Layout';
 import Avatar from 'components/ui/Avatar';
+import ContactEditForm from 'components/ContactEditForm';
 import { withData } from 'ladda-react';
 import withOwnerId from 'hocs/withOwnerId';
 
@@ -9,22 +10,66 @@ import api from 'api';
 
 import styles from './styles.scss';
 
-function ContactList({ contacts }) {
-  console.log(contacts);
-  return (
-    <div>
-      { map(contacts, (contact) => (
-        <Link key={ contact.id } to={ `edit/${contact.id}` } >
-          <Contact key={ contact.id } contact={ contact } />
-        </Link>
-      ))}
-    </div>
-  );
+class ContactList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inEditMode: {} };
+  }
+
+  setEditMode(id, nextState) {
+    this.setState((prevState) => ({
+      ...prevState,
+      inEditMode: {
+        ...prevState.inEditMode,
+        [id]: nextState
+      }
+    }));
+  }
+
+  render() {
+    const { contacts } = this.props;
+    const { inEditMode } = this.state;
+    return (
+      <div>
+        { map(contacts, (contact) => (
+          <Contact
+            key={ contact.id }
+            contact={ contact }
+            inEditMode={ inEditMode[contact.id] }
+            onEnterEditMode={ () => this.setEditMode(contact.id, true) }
+            onLeaveEditMode={ () => this.setEditMode(contact.id, false) }
+         />
+        ))}
+      </div>
+    );
+  }
 }
 
-function Contact({ contact }) {
+function Contact({ contact, inEditMode, onEnterEditMode, onLeaveEditMode }) {
+  if (inEditMode) {
+    const onSave = (updatedContact) => {
+      return api.contacts.updateContact({
+        ...contact,
+        ...updatedContact
+      }).then(() => onLeaveEditMode());
+    };
+
+    const onRemove = () => api.contacts.deleteContact(contact.id).then(() => onLeaveEditMode());
+
+    return (
+      <Card className={ styles.contact }>
+        <ContactEditForm
+          contact={ contact }
+          onSave={ onSave }
+          onRemove={ onRemove }
+          onCancel={ onLeaveEditMode }
+        />
+      </Card>
+    );
+  }
+
   return (
-    <Card className={ styles.contact }>
+    <Card className={ styles.contact } onClick={ onEnterEditMode }>
       <Avatar src={ contact.avatar } size="50" />
       <div className={ styles.details }>
         <div className={ styles.name }>{ contact.name }</div>
